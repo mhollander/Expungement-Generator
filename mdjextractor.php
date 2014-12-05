a72eec96fd2f62e6baaeed7e9fb2aa5dc1d45c05
Mike Hollander (work) <hollander@gmail.com>
2014-12-05 17:40:24 -0500

    Added Apache license header.

--- mdjextractor.php
+++ mdjextractor.php
@@ -1,8 +1,29 @@
 <?php
 
-//	mdjextractor.php
-//	reads the webpage http://www.pacourts.us/T/SpecialCourts/MDJList.htm
-// 	and extracts all of the court information for each MDJ number
+/*******************************************************
+*
+*	mdjextractor.php
+*	reads the webpage http://www.pacourts.us/T/SpecialCourts/MDJList.htm
+* 	and extracts all of the court information for each MDJ number
+*	NOTE: THIS IS A HELPER FILE THAT IS NOT USED REGULARLY FOR THE FUNCTIONING OF THE EG
+*
+*	Copyright 2011-2015 Community Legal Services
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************/
+
+
 require_once("config.php");
 require_once("head.php");
 
