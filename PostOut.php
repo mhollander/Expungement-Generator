33820f31bf6b0a7bfd71bc628016a626fd6eecb5
Mike Hollander (work) <hollander@gmail.com>
2015-03-25 17:13:33 -0400

    Added PostOut which just reflects posted variables back to the user.  Used to test legalserver integration.

---
+++ PostOut.php
@@ -0,0 +1,18 @@
+<html><body><table>
+<?php 
+
+
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+
+
+?>
+</table></body></html>
