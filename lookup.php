d3cc781362d5b315a6b4e61cdf1e4a6754fb88dc
Michael Hollander (badass) <hollander@gmail.com>
2016-04-07 01:39:41 +0000

    started new page that allows for searching of CPCMS, but not expunging.  This will be used for intake, etc...

---
+++ lookup.php
@@ -0,0 +1,111 @@
+<?php
+
+/*****************************************
+*
+*	index.php
+*	The head page for the expungement generator	
+*
+*	Copyright 2011-2015 Community Legal Services
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*********************************************/
+
+//	require_once("config.php");
+//	require_once("utils.php");
+    require_once("CPCMS.php");
+	include("head.php");
+
+	// the page header, including the menu bar, etc...
+//	include("header.php");
+
+$submit = false;
+
+// if this is from a submit, grab the post vars and place them into the form fields
+// also prepare to do the CPCMS search
+$first = "";
+$last = "";
+$dob = "";
+if (!empty($_POST))
+{
+    $submit = true;
+    $first = htmlspecialchars(stripslashes($_POST['personFirst']));
+    $last = htmlspecialchars(stripslashes($_POST['personLast']));
+    $dob = htmlspecialchars(stripslashes($_POST['personDOB']));
+}
+?>
+
+<div class="main">
+	<div class="pure-u-8-24">&nbsp;</div>
+	<div class="pure-u-12-24">
+		<form action="lookup.php" method="post" enctype="multipart/form-data">
+		<div class="form-item">
+			<label for="personFirst">Client's Name</label> <!--'-->
+			<div class="form-item-column">
+				<input type="text" name="personFirst" id="personFirst" class="form-text" value="<?php echo $first?> " />
+			</div>
+			<div class="form-item-column">
+				<input type="text" name="personLast" id="personLast" class="form-text" value="<?php echo $last?>" />
+			</div>
+			<div class="space-line"></div>
+			<div class="description">
+				<div class="form-item-column">
+					First Name
+				</div>
+				<div class="form-item-column">
+					Last Name
+				</div>
+			</div>
+			<div class="space-line"></div>
+		</div> 
+		<div class="form-item">
+		</div> 
+		<div class="form-item">
+		<label for="personDOB">Date of Birth</label>
+			<input type="date" name="personDOB" value="<?php echo $dob?>" maxlength="10"/>
+			<div class="description">MM/DD/YYYY</div>
+		</div>
+
+    	<div class="form-item">
+			<input type="submit" name="submit" value="Search CPCMS" />
+		</div>
+		</form>
+<?php
+// do the CPCMS search and print the results
+if ($submit)
+{
+    $cpcms = new cpcms($first, $last, $dob);
+    $status = $cpcms->cpcmsSearch();
+    $statusMDJ = $cpcms->cpcmsSearch(true);
+    if (!preg_match("/0/",$status[0]) && !preg_match("/0/", $statusMDJ[0]))
+    {                                                                      
+        print "<br/><b>Your search returned no results.  This is probably because there is no one with the name $first $last in the court database.  You can try searching <a href='http://ujsportal.pacourts.us' target='_blank'>CPCMS directly</a> if you think that there is some error.";
+        print $status[0];
+        print "<br/>" . $statusMDJ[0]; 
+    }
+    else
+    {
+        //only integrate the summary information if we have a DOB; otherwise what is the point?
+        if (!empty($dob))                                                                        
+            $cpcms->integrateSummaryInformation();
+         $cpcms->displayAsWebForm(basename(__FILE__), $_POST);
+    }
+}
+?>
+	</div>
+	<div class="content-right">&nbsp;</div>
+</div>
+
+<?php 
+	include("foot.php");
+?>
