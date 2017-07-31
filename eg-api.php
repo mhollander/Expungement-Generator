0fd304c81b4c796f5ae5d22965150ed360fcf805
NateV <nvogel@sparkles>
2017-07-31 16:16:47 +0000

    formatting response object as json

--- eg-api.php
+++ eg-api.php
@@ -1,48 +1,38 @@
 
 
 <?php
-	//print_r("getting started");
 	include('CPCMS.php');
 	require_once('utils.php');
 	require_once('config.php');
-	//require_once('dbconnect.php');
 	include('expungehelpers.php');
 	//initialize the response that will get sent back to requester
-	//print_r("everything included.");
-	$response = "response not clear yet.";
+	$response = array();
 
-	//session_start();
-	
 	if(!validAPIKey()) {
-		$response = "403 - bad api key.";
+		$response['results']['status'] = "403 - bad api key.";
 	} else {
 		// a cpcmsSearch flag can be set to true in the post request
 		// to trigger a cpcms search.
 		if (isset($_POST['cpcmsSearch']) && $_POST['cpcmsSearch']=='true'){
-			print("beginning cpcms Search\n");
 			$urlPerson = getPersonFromPostOrSession();
+
 			$cpcms = new CPCMS($urlPerson['First'],$urlPerson['Last'], $urlPerson['DOB']);
 			$status = $cpcms->cpcmsSearch();
 			$statusMDJ = $cpcms->cpcmsSearch(true);
 			if (!preg_match("/0/",$status[0]) && !preg_match("/0/", $statusMDJ[0])) {
-				print("The CPCMS search returned no results");
-				$response = "Your CPCMS search returned no results.";
+				$response['results'] = "Your CPCMS search returned no results.";
 			} else {
 				//only integrate the summary information if we
         // have a DOB; otherwise what is the point?
-				print("The CPCMS Search returned results!\n");
         			if (!empty($urlPerson['DOB'])) {
             				$cpcms->integrateSummaryInformation();
 				}
-				print_r($cpcms);
 				// We need an array of docket numbers, so we take the list of results
 				// and extract only the docket number from each.
 				$docketNums = array();
 				foreach (array_merge($cpcms->getResults(), $cpcms->getMDJResults()) as $result) {
 					$docketNums[] = $result[0];
 				};
-				print("Docket numbers:\n");
-				print_r($docketNums);
         // remove the cpcmsSearch variable from the POST vars and then pass them to
         // a display funciton that will display all of the arrests as a webform, with all
         // of the post vars re-posted as hidden variables.  Also pass this filename as the
@@ -63,17 +53,15 @@
 												 $urlPerson['State'],
 												 $urlPerson['Zip']);
 	  	
-		//print_r($urlPerson);
-		//print_r($person);
 		getInfoFromGetVars(); //this sets session variables based on the GET or
 		// POST variables 'docket', 'act5Regardless', 'expungeRegardless', and
 		// 'zipOnly'
-
+		$response['personFirst'] = $urlPerson['First'];
+		$response['personLast'] = $urlPerson['Last'];
+		$response['dob'] = $urlPerson['DOB'];
 		$attorney = new Attorney($_POST['useremail'], $db);
 
 		$docketFiles = $_FILES;
-		//print("docketNums has been set.\n");
-		//print_r($docketNums);
 		
 		if (!isset($docketNums)) {
 			// If $docketNums wasn't set in CPCMS search, initialize it to an empty array.
@@ -86,32 +74,36 @@
 			foreach (explode(",",$_POST['docketNums']) as $doc) {
 				array_push($docketNums, $doc);
 			}
+			$response['results']['dockets'] = $docketNums;
 		}
-
-		print_r($docketNums);
 
 		if (count($docketNums)>0) {
 			//if the cpcms search has been run and has found dockets
 			$docketFiles = CPCMS::downloadDockets($docketNums);
 			$arrests = parseDockets($tempFile, $pdftotext, $arrestSummary, $person, $docketFiles);
-			integrateSummaryInformation($arrests, $person, $arrestSummary);
+			integrateSummaryInformation($arrests, $person, $arrestSummary, True);
+			//set $isAPI in integrateSummaryInformation() to True to prevent printing to screen
 			$arrests = combineArrests($arrests);
-			print("Arrests has been set:\n");
-			print_r($arrests);
+			$response['results']['arrestCount'] = count($arrests);
+			# TODO Could add a function to insert a string of arrest information into $response.
+			# TODO Could also add a function to insert information about chargeObjects (child of Arrest)
 		}
 		$sealable = checkIfSealable($arrests);
-
+		// doExpungements prints a table to the screen. That's not desired here, so this method wraps the function 
+		// in an OutputBuffer to prevent that.
+		ob_start();
 		$files = doExpungements($arrests, $templateDir, $dataDir, $person,
 														$attorney, $_SESSION['expungeRegardless'],
 														$db, $sealable);
+		ob_end_clean();
 		$files[] = createOverview($arrests, $templateDir, $dataDir, $person, $sealable);
 		$zipFile = zipFiles($files, $dataDir, $docketFiles,
 								        $person->getFirst() . $person->getLast() . "Expungements");
 
 		if (count($files) > 0) {
-			$response = $baseURL . "data/" . basename($zipFile);
+			$response['results']['expungeZip'] = $baseURL . "data/" . basename($zipFile);
 		} else {
-			$response = "Error. No dockets downloaded. It would be nice if this message were more helpful.";
+			$response['results']['status'] = "Error. No dockets downloaded. It would be nice if this message were more helpful.";
 		}
 
 
@@ -125,38 +117,25 @@
 		cleanupFiles($files);
 	}// end of processing req from a valid user
 	
-	print("Here is the response:\n");
-	print($response);
+	print_r(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES, 3));
 
 	function validAPIKey() {
-		//print("Testing validity of apikey");
-		//print_r($_POST);
 		$db = $GLOBALS['db'];
 		if (!isset($db)) {
-			//print("database still not accessible!");
 		} else {
-			//print_r($db);
 		}
 		if (!isset($_POST['useremail'])) {
-			//print("useremail not included in POST request.");
 			return False;
 		}
 		$useremail = $_POST['useremail'];
 		if (isset($_POST['apikey'])) {
-			//print("Hashing " . $_POST['apikey']);
-			//print($db->real_escape_string($_POST['useremail']));
 			$query = "SELECT user.apikey FROM user WHERE user.email = '".$useremail."';";
-			//print("\n" . $query . "\n");
 			$result = $db->query($query);
-			//print_r($result);
 			if (!$result) {
-				print("could not get api hash for user email from db");
 				return False;
 			};
 			$row = mysqli_fetch_assoc($result);
-			//print_r($row);
 			if (password_verify($_POST['apikey'], $row['apikey'])) {
-			//print("API Key verified!!\n\n");
 			return True;
 			};
 		}; 
