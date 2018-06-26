e2cdd509ebbd83511387d92403aa286001a483da
Nate Vogel <nvogel@clsphila.org>
2018-06-26 15:06:13 -0400

    fix: fixed error messages when cpcms searches find nothing

--- Expungement-Generator/expunge.php
+++ Expungement-Generator/expunge.php
@@ -5,12 +5,12 @@
 /***********************************************************************
 *
 *	expunge.php
-*	The main controller for actually completing the expungements.  Deals with 
+*	The main controller for actually completing the expungements.  Deals with
 *   dump of all docket sheets and summary sheet, sends them all to be parsed,
 *	combines information as needed, and then makes calls to generate reports.
 *
 *	Copyright 2011-2015 Community Legal Services
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -58,7 +58,7 @@ else if (isset($_POST['cpcmsSearch']) && $_POST['cpcmsSearch'] == "true")
     $cpcms = new CPCMS($urlPerson['First'], $urlPerson['Last'], $urlPerson['DOB']);
     $status = $cpcms->cpcmsSearch();
     $statusMDJ = $cpcms->cpcmsSearch(true);
-    if (!preg_match("/0/",$status[0]) && !preg_match("/0/", $statusMDJ[0]))
+    if (!preg_match("/Status: 0/",$status[0]) && !preg_match("/Status: 0/", $statusMDJ[0]))
     {
          print "<br/><b>Your search returned no results.  This is probably because there is no one with the name '" . $urlPerson['First'] . " " . $urlPerson['Last'] . "' in the court database.</b><br/><br/>  The other possibliity is that CPCMS is down.  You can press back and try your search again or you can check <a href='https://ujsportal.pacourts.us/DocketSheets/CP.aspx' target='_blank'>CPCMS by clicking here and doing your search there</a>.</b>";
         print $status[0];
@@ -70,59 +70,59 @@ else if (isset($_POST['cpcmsSearch']) && $_POST['cpcmsSearch'] == "true")
         // have a DOB; otherwise what is the point?
         if (!empty($urlPerson['DOB']))
             $cpcms->integrateSummaryInformation();
-        
+
         // remove the cpcmsSearch variable from the POST vars and then pass them to
         // a display funciton that will display all of the arrests as a webform, with all
-        // of the post vars re-posted as hidden variables.  Also pass this filename as the 
+        // of the post vars re-posted as hidden variables.  Also pass this filename as the
         // form action location.
         unset($_POST['cpcmsSearch']);
-        
+
         $cpcms->displayAsWebForm(basename(__FILE__));
     }
 }
 
-else 
+else
 {
     if (isset($_POST['docket']))
        $_SESSION['docket'] = $_POST['docket'];
     if (isset($_POST['scrapedDockets']))
        $_SESSION['scrapedDockets'] = $_POST['scrapedDockets'];
-    
+
 	$arrests = array();
 	$arrestSummary = new ArrestSummary();
 
 	// get information about the person from the POST vars passed in
 	$urlPerson = getPersonFromPostOrSession();
 	$person = new Person($urlPerson['First'], $urlPerson['Last'], $urlPerson['SSN'], $urlPerson['Street'], $urlPerson['City'], $urlPerson['State'], $urlPerson['Zip']);
-	
+
     // set some session vars based on get and post vars
     getInfoFromGetVars();
-              
+
 	// make sure to change this in the future to prevent hacking!
 	$attorney = new Attorney($_SESSION["loginUserID"], $db);
 	if($GLOBALS['debug'])
 		$attorney->printAttorneyInfo();
-	
+
 	// parse the uploaded files will lead to expungements or redactions
     $docketFiles = $_FILES;
     if (isset($_SESSION['scrapedDockets']))
         $docketFiles = CPCMS::downloadDockets($_SESSION['docket']);
-      
+
 	$arrests = parseDockets($tempFile, $pdftotext, $arrestSummary, $person, $docketFiles);
-	
-	// integrate the summary information in with the arrests 
+
+	// integrate the summary information in with the arrests
 	integrateSummaryInformation($arrests, $person, $arrestSummary);
-	
+
 	print "<b>EXPUNGEMENT INFORMATION</b><br/><br/>";
 	// combine the docket sheets that are from the same arrest
 	$arrests = combineArrests($arrests);
-    
+
     // check to see if Act5 Sealable
     $sealable = checkIfSealable($arrests);
-		
+
 	// do the expungements in PDF form
 	$files = doExpungements($arrests, $templateDir, $dataDir, $person, $attorney, $_SESSION['expungeRegardless'], $db, $sealable);
-	
+
 
 	$files[] = createOverview($arrests, $templateDir, $dataDir, $person, $sealable);
 
@@ -136,17 +136,17 @@ else
 		print "<div><b>No expungeable or redactable offenses found for this individual.</b></div>";
 
 	// write everything to the DB as long as this wasn't a "test" upload.
-	// we determine test upload if a SSN is entered.  If there is no SSN, we assume that 
+	// we determine test upload if a SSN is entered.  If there is no SSN, we assume that
 	// there was no expungement either - it was just a test to see whether expungements were
 	// possible or a test of the generator itself by yours truly.
 	if (isset($urlPerson['SSN']) && $urlPerson['SSN'] != "")
 		writeExpungementsToDatabase($arrests, $person, $attorney, $db);
-	
-	
+
+
 	// if we are debuging, display the expungements
-	if ($GLOBALS['debug']) 
+	if ($GLOBALS['debug'])
 		screenDisplayExpungements($arrests);
-		
+
 	// cleanup any files that are left over
 	cleanupFiles($files);
 } // if isLoggedIn()
@@ -157,4 +157,3 @@ else
 
 <?php
 include ('foot.php');
-	
